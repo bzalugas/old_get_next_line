ec4caa9f264bcc50b6cfba69b4ec2b198ebe3a01
Bastien Zalugas <bastien.zalugas@hotmail.fr>
2021-06-21 21:51:39 +0200

    Change return of get_the_line & change of structure of get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,17 @@
 /*   By: bzalugas <bzalugas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 18:57:41 by bzalugas          #+#    #+#             */
-/*   Updated: 2021/06/15 18:17:38 by bzalugas         ###   ########.fr       */
+/*   Updated: 2021/06/21 21:50:28 by bzalugas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
+//#include "leaks_tester.h"
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 0
 #endif
+
+//extern t_list *liste;
 
 int     find(char c, char *str, size_t start)
 {
@@ -30,62 +32,101 @@ int     find(char c, char *str, size_t start)
     return i;
 }
 
-char    *get_the_line_and_cut_text(char **text, int end_line)
+int     get_the_line(char **line, char **text, int end_line)
 {
-    char *line;
 
-    if (end_line == -1 && (end_line = find('\n', *text, 0)) == -1)
-        end_line = ft_strlen(*text);
-    line = ft_substr(*text, 0, end_line);
+/*    if (end_line == -1 && (end_line = find('\n', *text, 0)) == -1)
+        end_line = ft_strlen(*text);*/
+//    printf("end line : %d\n", end_line);
+//    printf("len : %lu\n", ft_strlen(*text));
+    *line = ft_substr(*text, 0, end_line);
     *text = ft_substr(*text, end_line + 1, ft_strlen(*text));
-    return (line);
+    return (1);
 }
 
 int     get_next_line(int fd, char **line)
 {
-    //char        buff[BUFFER_SIZE + 1];
-    char        *buff;
+    char        buff[BUFFER_SIZE + 1];
     static char *text = NULL;
-    static int  result = BUFFER_SIZE;
+    int         result;
     int         end_line;
 
-    buff = malloc(sizeof(char) * BUFFER_SIZE + 1);
-    if (buff == NULL || fd < 0 || !line || BUFFER_SIZE < 1 || read(fd, buff, 0) < 0)
+    if (fd < 0 || !line || BUFFER_SIZE < 1 || read(fd, buff, 0) < 0)
         return (-1);
-    end_line = -1;
-    while ((result != 0 || result == BUFFER_SIZE) && end_line < 0)
+    if (text && (end_line = find('\n', text, 0)) != -1)
+        return (get_the_line(line, &text, end_line));
+    while ((result = read(fd, buff, BUFFER_SIZE)) > 0)
     {
-        result = read(fd, buff, BUFFER_SIZE);
+        //printf("boucle %d\n", nb++);
         buff[result] = '\0';
         if (text == NULL)
-            text = malloc(sizeof(char) * BUFFER_SIZE + 1);
-        if (text == NULL)
-            return (-1);
-        text = ft_strjoin(text, buff);
-        end_line = find('\n', text, 0);
+            if (!(text = malloc(sizeof(char) * BUFFER_SIZE + 1)))
+                return (-1);
+        text = ft_strjoin(text, buff); //modifier le strjoin pour qu'il fonctionne meme si text == NULL
+        if ((end_line = find('\n', text, 0)) != -1)
+            return (get_the_line(line, &text, end_line));
     }
-    *line = get_the_line_and_cut_text(&text, end_line);
-    free(buff);
-    if (text && text[0] == '\0' && result < BUFFER_SIZE)
-        return (0);
-    return (1);
+    if (!text)
+    {
+        *line = ft_strdup(text);
+        free(text);
+        return (result);
+    }
+    *line = ft_strdup("");
+    return (result);
+//    printf("text[0] = %c\n", text[0]);
+
+/*    if (!(text && text[0] == '\0' && result < BUFFER_SIZE))
+        return (1);
+    free(text);
+    return (0); */
+
+//    if (text && text[0] == '\0' && result < BUFFER_SIZE)
+/*    if (text && text[0] == '\0')
+    {
+        if (read(fd, buff, 1) == 0)
+        {
+            free(text);
+            return (0);
+        }
+        text = ft_strjoin(text, buff);
+    }
+    return (1);*/
 }
 
 #include <fcntl.h>
 
-
-int main()
+void afficher(void *content)
 {
-    int fd = open("./test2.txt", O_RDONLY);
+    printf("%p[%lu]\n", content, sizeof(content));
+}
+
+void delete(void *content)
+{
+    content = NULL;
+}
+
+int main(int argc, char **argv)
+{
+    (void)argc;
+    char *name = argv[1];
+    int fd = open(name, O_RDONLY);
     char *line;
-    if (!(line = malloc(sizeof(char) * BUFFER_SIZE)))
-        exit(0);
     int result = 1;
+    printf("Contenu de %s : \n\n", name);
     while (result > 0)
     {
         result = get_next_line(fd, &line);
-        printf("next line : %s\n", line);
+//        printf("next line : %s(result = %d)\n", line, result);
+        printf("%s\n", line);
     }
     free(line);
     close(fd);
+/*    printf("adresses restantes : \n");
+    void (*aff)(void *);
+    aff = &afficher;
+    ft_lstiter(liste, aff);
+    void (*del)(void *);
+    del = &delete;
+    ft_lstclear(&liste, del);*/
 }
